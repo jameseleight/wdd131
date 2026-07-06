05b42535e19ba72625bdec1543d14086d5a3f1c6
jameseleight <jleight@byupathway.edu>
2026-07-06 00:35:46 -0700

    update file location

---
+++ scripts/getdates.js
@@ -0,0 +1,16 @@
+// declare a new date object and use it to display the Year
+const today = new Date();
+document.getElementById("currentyear").innerHTML = today.getFullYear();
+
+// declare a new date object created from the lastModified method string and display it as a formatted time
+const lastMod = new Date(document.lastModified);
+document.getElementById("lastModified").innerHTML = lastMod.toLocaleString("en-US", {
+    year: "numeric",
+    month:"long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "2-digit",
+    second: "2-digit",
+    weekday: "long",
+    timeZoneName: "short"
+});
